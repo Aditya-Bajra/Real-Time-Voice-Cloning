b85accd4d80a3ad3f9167695a26f374fcbead94e
projan-shakya <84801161+projan-shakya@users.noreply.github.com>
2024-02-23 10:04:08 +0545

    first pusg

---
+++ encoder/config.py
@@ -0,0 +1,17 @@
+librispeech_datasets = {
+    "train": {
+        "clean": ["LibriSpeech/train-other-100"],
+        "other": ["LibriSpeech/train-other-100"]
+    },
+    "test": {
+        "clean": ["LibriSpeech/train-other-100"],
+        "other": ["LibriSpeech/train-other-100"]
+    },
+    "dev": {
+        "clean": ["LibriSpeech/train-other-100"],
+        "other": ["LibriSpeech/train-other-100"]
+    },
+}
+
+
+anglophone_nationalites = ["nepal"]
