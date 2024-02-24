3b7970325b5011ffb3954d196b7b3ee721a7e87b
Aditya_Bajracharya <84086387+Aditya-Bajra@users.noreply.github.com>
2024-02-24 13:28:33 +0545

    Update train.py

--- synthesizer/train.py
+++ synthesizer/train.py
@@ -11,7 +11,7 @@ from synthesizer import audio
 from synthesizer.models.tacotron import Tacotron
 from synthesizer.synthesizer_dataset import SynthesizerDataset, collate_synthesizer
 from synthesizer.utils import ValueWindow, data_parallel_workaround
-from synthesizer.utils.plot import plot_spectrogram
+from synthesizer.utils.plot import plot_spectrogram, plot_alignment
 from synthesizer.utils.symbols import symbols
 from synthesizer.utils.text import sequence_to_text
 from vocoder.display import *
